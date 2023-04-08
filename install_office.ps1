a80e3705738503ad4d54c783f6f4b2f6c8feabbb
upioneer <hgrand@outlook.com>
2023-04-08 14:17:01 -0500

    Completed and tested the script

--- install_office.ps1
+++ install_office.ps1
@@ -1,16 +1,23 @@
 <#
-References:
-    https://config.office.com
-    https://www.microsoft.com/en-us/download/details.aspx?id=49117
-    https://learn.microsoft.com/en-us/deployoffice/ltsc2021/deploy
-    https://id.loc.gov/vocabulary/iso639-1.html
+    References:
+        https://config.office.com
+        https://www.microsoft.com/en-us/download/details.aspx?id=49117
+        https://learn.microsoft.com/en-us/deployoffice/ltsc2021/deploy
+        https://id.loc.gov/vocabulary/iso639-1.html
 #>
-$folder = 'C:\OfficeProPlus2021'
 
+# create a working directory
+$folder = 'C:\OfficeProPlus2021'
+if (!(Test-Path $folder)) {
+    New-Item -ItemType Directory -Path $folder
+}
+
+# capture current firewall state for domain, private and public profiles
 $fwdomain = Get-NetFirewallProfile -Profile domain
 $fwprivate = Get-NetFirewallProfile -Profile private
 $fwpublic = Get-NetFirewallProfile -Profile public
 
+# temporarily disable firewall for domain, private and public profiles
 Set-NetFirewallProfile -Profile domain -Enabled False
 Set-NetFirewallProfile -Profile private -Enabled False
 Set-NetFirewallProfile -Profile public -Enabled False
@@ -32,27 +39,71 @@ $regkeys = @(
     'HKLM:\Software\Microsoft\Office\Software'
 )
 
+# delete existing office keys known to possibly conflict
 foreach ($regkey in $regkeys) {
     if (Test-Path $regkey) {
         Remove-Item $regkey -Force -Verbose -Recurse
     }
 }
 
-if (!(Test-Path $folder)) {
-    New-Item -ItemType Directory -Path $folder
-}
+# scrape for the dynamic download link
+$web = Invoke-WebRequest -Uri 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117'
 
-Invoke-WebRequest -uri 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_16026-20170.exe' -OutFile "$folder\officedeploymenttool_16026-20170.exe"
+# extract filename from download link
+$downloadlink = $web.links | Where-Object -Property 'href' -Like "*.exe"
+$exe = $downloadlink.href.Split("/")[-1]
+
+# download office deployment tool
+$destination = "$folder\$exe"
+Start-BitsTransfer -Source $downloadlink.href -Destination $destination
+
+# extract setup.exe from office deployment tool
 $arguments = "/extract:$folder /quiet /norestart"
-Start-Process "$folder\officedeploymenttool_16026-20170.exe" -ArgumentList $arguments -Wait
+Start-Process "$folder\$exe" -ArgumentList $arguments -Wait
 
+# remove .xml files
 Get-ChildItem $folder | foreach {Remove-Item -Path $_.FullName -Include "*.xml"}
 
-Invoke-WebRequest -uri 'https://github.com/upioneer/Microsoft-Office-2021/blob/main/configuration/en-Configuration.xml' -OutFile "$folder\configuration.xml"
+# xml details
+$xml = '<Configuration ID="81e8f972-43e9-4911-afde-baa299853e5a">
+  <Add OfficeClientEdition="64" Channel="PerpetualVL2021">
+    <Product ID="ProPlus2021Volume" PIDKEY="FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH">
+      <Language ID="en-us" />
+      <ExcludeApp ID="Lync" />
+    </Product>
+    <Product ID="VisioPro2021Volume" PIDKEY="KNH8D-FGHT4-T8RK3-CTDYJ-K2HT4">
+      <Language ID="en-us" />
+      <ExcludeApp ID="Lync" />
+    </Product>
+    <Product ID="ProjectPro2021Volume" PIDKEY="FTNWT-C6WBT-8HMGF-K9PRX-QV9H8">
+      <Language ID="en-us" />
+      <ExcludeApp ID="Lync" />
+    </Product>
+    <Product ID="LanguagePack">
+      <Language ID="en-us" />
+    </Product>
+  </Add>
+  <Property Name="SharedComputerLicensing" Value="0" />
+  <Property Name="FORCEAPPSHUTDOWN" Value="FALSE" />
+  <Property Name="DeviceBasedLicensing" Value="0" />
+  <Property Name="SCLCacheOverride" Value="0" />
+  <Property Name="AUTOACTIVATE" Value="1" />
+  <Updates Enabled="TRUE" />
+  <RemoveMSI />
+  <AppSettings>
+    <User Key="software\microsoft\office\16.0\excel\options" Name="defaultformat" Value="51" Type="REG_DWORD" App="excel16" Id="L_SaveExcelfilesas" />
+    <User Key="software\microsoft\office\16.0\powerpoint\options" Name="defaultformat" Value="27" Type="REG_DWORD" App="ppt16" Id="L_SavePowerPointfilesas" />
+    <User Key="software\microsoft\office\16.0\word\options" Name="defaultformat" Value="" Type="REG_SZ" App="word16" Id="L_SaveWordfilesas" />
+  </AppSettings>
+  <Display Level="Full" AcceptEULA="TRUE" />
+</Configuration>'
+$xml | Out-File $folder\configuration.xml -Force
 
+# install office pro plus
 $arguments = "/configure $folder\configuration.xml"
 Start-Process "$folder\setup.exe" -ArgumentList $arguments -Wait
 
+# restore firewall state for domain, private and public profiles
 Set-NetFirewallProfile -Profile domain -Enabled $fwdomain.Enabled
 Set-NetFirewallProfile -Profile private -Enabled $fwprivate.Enabled
 Set-NetFirewallProfile -Profile public -Enabled $fwpublic.Enabled
